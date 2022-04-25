6bdb821241b2a143d8a4867d78ce8a1dd62567e3
Jean-Baptiste WITTNER <jb.wittner@outlook.com>
2022-04-25 16:40:10 +0200

    pmd remarks

--- src/main/java/fr/myhome/server/MyHomeServerApplication.java
+++ src/main/java/fr/myhome/server/MyHomeServerApplication.java
@@ -1,4 +1,4 @@
-package fr.myhome.Server;
+package fr.myhome.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MyHomeServerApplication {
 
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
 		SpringApplication.run(MyHomeServerApplication.class, args);
 	}
 
--- src/test/java/fr/myhome/server/MyHomeServerApplicationTests.java
+++ src/test/java/fr/myhome/server/MyHomeServerApplicationTests.java
@@ -1,4 +1,4 @@
-package fr.myhome.Server;
+package fr.myhome.server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
