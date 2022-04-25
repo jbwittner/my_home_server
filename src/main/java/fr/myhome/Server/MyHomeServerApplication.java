fd441b5ccffed47f590a1ce1bcc18cc34ed3786b
Jean-Baptiste WITTNER <jb.wittner@outlook.com>
2022-04-25 15:34:21 +0200

    Init

---
+++ src/main/java/fr/myhome/Server/MyHomeServerApplication.java
@@ -0,0 +1,13 @@
+package fr.myhome.Server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyHomeServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyHomeServerApplication.class, args);
+	}
+
+}
